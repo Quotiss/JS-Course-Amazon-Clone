c19168d54b474ba21d18d84085c72204db51b79d
Quotiss <noahgordon2020@yahoo.com>
2024-08-22 12:45:53 -0500

    Use MVC to change checkout header when deleting product from cart

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,6 +5,7 @@ import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import {renderPaymentSummary} from './paymentSummary.js';
+import {renderCheckoutHeader} from './checkoutHeader.js';
 
 export function renderOrderSummary() {
 
@@ -125,6 +126,8 @@ export function renderOrderSummary() {
       link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+
+        renderCheckoutHeader();
 
         renderOrderSummary();
 
