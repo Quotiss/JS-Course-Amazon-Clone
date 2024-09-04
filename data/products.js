5acb4431747714aa228039e872d072807558f855
Quotiss <noahgordon2020@yahoo.com>
2024-09-04 13:13:08 -0500

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -129,6 +129,8 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -156,6 +158,10 @@ export function loadProducts(fun) {
     console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
