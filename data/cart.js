86464e0ab6ae7d20d4c56dd37a9be743a41b6e97
Quotiss <noahgordon2020@yahoo.com>
2024-09-28 11:46:47 -0500

    Extra functionality

--- data/cart.js
+++ data/cart.js
@@ -6,16 +6,7 @@ export function loadFromStorage() {
   cart = JSON.parse(localStorage.getItem('cart'));
 
   if (!cart) {
-    cart = [{
-      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2,
-      deliveryOptionId: '1'
-    },
-    {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1,
-      deliveryOptionId: '2'
-    }];
+    cart = [];
   }
 }
 
@@ -122,4 +113,9 @@ export async function loadCartFetch() {
   const text = await response.text();
   console.log(text);
   return text;
+}
+
+export function resetCart() {
+  cart = [];
+  saveToStorage();
 }
